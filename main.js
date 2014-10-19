dbfee0abfdecd3c68f53d512911eb7f1478203d9
Volodymyr Siedlecki <siedleck@live.unc.edu>
2014-10-19 01:38:55 -0400

    added color change effect :D

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+
 $( document ).ready( function(){
      $("#description").hide();
+     setInterval(function(){changeTitleColor()}, 1000)
 });
 function showGame(){
     $("#members").fadeToggle(200);
@@ -9,3 +11,12 @@ function showGame(){
     },300);
        return false;
     };
+    
+function changeTitleColor() {
+   var red = Math.floor(Math.random() * 255);
+   var green = Math.floor(Math.random() * 255);
+   var blue = Math.floor(Math.random() * 255);
+   var rgb = "rgb("+red+","+green+","+blue+")";
+              
+   $('#title').css("color", rgb)
+}
