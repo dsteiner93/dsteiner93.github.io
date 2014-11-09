a557722659e68d86f7675366b89ad8e58fd596f5
Volodymyr Siedlecki <volosied@gmail.com>
2014-11-09 15:32:21 -0500

    updated for the demo

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
-
-$( document ).ready( function(){
-     $("#gameArea").hide();
-     setInterval(function(){changeTitleColor()}, 1000)
+$(document).ready(function() {
+    $("#gameArea").hide();
+    $("#demoCanvas").hide();
+    $("#center").hide();
+    //setInterval(function(){changeTitleColor()}, 1000)
 });
-function showRules(){
+
+function showGame() {
     $("#members").fadeToggle(200);
     $("#start").fadeToggle(200);
-    setTimeout(function(){
-          $("#board").hide();
-        $("#gameArea").css("height", $("#text").css("height"));
-    $("#gameArea").fadeIn('slow');
-    },300);
-       return false;
-    };
-   
+    setTimeout(function() {
+        $("#gameArea").fadeIn('slow');
+    }, 300);
+    return false;
+};
 
-/*   Just commenting this out to test it out. We can uncomment later - David
-function changeTitleColor() {
-   var red = Math.floor(Math.random() * 255);
-   var green = Math.floor(Math.random() * 255);
-   var blue = Math.floor(Math.random() * 255);
-   var rgb = "rgb("+red+","+green+","+blue+")";
-              
-   $('#title').css("color", rgb)
-}*/
 
-function showGame(){
-       $("#text").fadeToggle(300);
-        setTimeout(function() {$("#board").fadeIn(300);}, 300);
-          $('#gameArea').css("width", 850);
-}
+function showCanvas() {
+    $("#players").fadeToggle(200);
+    setTimeout(function() {
+        $("#center").fadeIn('slow');
+        $("#demoCanvas").fadeIn('slow');
+        $( "#bubble" ).toggle( "scale" );
+    }, 300);
+};
+
+
+
+
+//   Just commenting this out to test it out. We can uncomment later - David
+//function changeTitleColor() {
+//   var red = Math.floor(Math.random() * 255);
+//   var green = Math.floor(Math.random() * 255);
+//   var blue = Math.floor(Math.random() * 255);
+//   var rgb = "rgb("+red+","+green+","+blue+")";
+//              
+//   $('#title').css("color", rgb)
+//}
