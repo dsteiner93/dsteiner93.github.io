7514e190918365f45a8c95cb13ced14ee704b1ed
dsteiner93 <dsteiner93@gmail.com>
2014-11-10 12:59:36 -0500

    test

--- puyo.js
+++ puyo.js
@@ -318,7 +318,7 @@
 			for(x = 0; x<=5; x++){ //for each column
 				condenseColumn(x);
 			}
-			setTimeout(updateBoard(0), 1000);
+			setTimeout(updateBoard(0), 10000);
 		}
 		
 		function condenseColumn(x) {
