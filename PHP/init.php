279881c200e98277214b00a2015ebea174cd93ae
Volodymyr Siedlecki <volosied@gmail.com>
2014-12-04 16:45:20 -0500

    Update init.php

--- PHP/init.php
+++ PHP/init.php
@@ -14,7 +14,7 @@ $db;
 //$db = new PDO("mysql:host=$host_name;dbname=db", $user_name, $pass_word);
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=siedleckdb", $user, $pwd);
+    $db = new PDO("mysql:host=$host;dbname=databaseName", $user, $pwd);
     /*** echo a message saying we have connected ***/
    
 }
